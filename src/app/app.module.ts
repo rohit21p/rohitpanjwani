53d3460ef57312f8326951216675819d9879fa45
Rohit <iamrohit.panjwani@gmail.com>
2019-07-22 11:14:35 +0530

    about me component generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from 'src/app/nav-bar/nav-bar.component';
 import { HomeComponent } from 'src/app/home/home.component';
+import { AboutMeComponent } from 'src/app/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule
