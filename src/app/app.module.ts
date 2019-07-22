4b96526e8556601de189865b2d2991e74abc2072
Rohit <iamrohit.panjwani@gmail.com>
2019-07-22 11:19:59 +0530

    resume component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from 'src/app/nav-bar/nav-bar.component';
 import { HomeComponent } from 'src/app/home/home.component';
 import { AboutMeComponent } from 'src/app/about-me/about-me.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HomeComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule
